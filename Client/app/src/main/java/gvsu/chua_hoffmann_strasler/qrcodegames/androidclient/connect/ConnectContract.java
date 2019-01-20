c73db86882fa960702ee5132c09d3d7e3691530c
Josh Chua <josh@joshchua.com>
2019-01-19 23:33:18 -0500

    Created Client Project with MVP starter code.  Kryonet maven projects imported.

---
+++ Client/app/src/main/java/gvsu/chua_hoffmann_strasler/qrcodegames/androidclient/connect/ConnectContract.java
@@ -0,0 +1,14 @@
+package gvsu.chua_hoffmann_strasler.qrcodegames.androidclient.connect;
+
+import gvsu.chua_hoffmann_strasler.qrcodegames.androidclient.BasePresenter;
+import gvsu.chua_hoffmann_strasler.qrcodegames.androidclient.BaseView;
+
+public interface ConnectContract {
+    interface View extends BaseView<Presenter> {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
+}
---
+++ Server/src/main/java/QRCodeGameServer.java
@@ -0,0 +1,7 @@
+import com.esotericsoftware.kryonet.*;
+
+public class QRCodeGameServer {
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+}
